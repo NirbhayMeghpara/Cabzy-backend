02af03326d62b9903880334cd46ff11d49a08c60
Nirbhay_Elluminati <nirbhay.elluminati@gmail.com>
2023-09-27 19:25:40 +0530

    City feature completed with editZone functionality

--- src/controller/cityController.js
+++ src/controller/cityController.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const City = require("../models/city")
 
 async function add(req, res) {
@@ -5,10 +6,12 @@ async function add(req, res) {
     if (Object.keys(req.body).length === 0) {
       throw new Error("Please enter a valid city")
     }
+    const places = req.body.location.split(",")
+    const cityName = places[0].trim()
 
-    const city = await City(req.body)
+    const city = await City({ name: cityName, ...req.body })
     await city.save()
-    res.send({ msg: `${req.body.name} created successfully !!` })
+    res.send({ msg: `${cityName} created successfully !!` })
   } catch (error) {
     if (error.keyValue) {
       const places = req.body.location.split(",")
@@ -24,17 +27,61 @@ async function add(req, res) {
 async function fetchCity(req, res) {
   try {
     const country = decodeURIComponent(req.params.country).toLowerCase();
+    const page = parseInt(req.query.page) || 1;
+    const limit = 4;
 
-    const cities = await City.aggregate([{ $match: { country } }])
-    if (!cities.length) {
+    const skip = (page - 1) * limit;
+
+    const result = await City.aggregate([
+      { $match: { country } },
+      {
+        $facet: {
+          data: [
+            { $count: 'totalCities' }
+          ],
+          cities: [
+            { $skip: skip },
+            { $limit: limit }
+          ]
+        }
+      }
+    ]);
+
+    if (!result[0].cities.length) {
       res.status(404).send({ msg: "No City Available" })
       return
     }
+    const cityCount = result[0].data[0].totalCities
+    const cities = result[0].cities
 
-    res.send(cities)
+    res.send({ cityCount, cities })
   } catch (error) {
     res.status(500).send({ error: error.message })
   }
 }
 
-module.exports = { add, fetchCity }
+async function edit(req, res) {
+  try {
+    if (Object.keys(req.body).length === 0) {
+      throw new Error("Please provide valid input")
+    }
+    const places = req.body.location.split(",")
+    const cityName = places[0].trim()
+
+    const _id = new mongoose.Types.ObjectId(req.body.id);
+    const city = await City.findById(_id)
+    if (!city) {
+      res.status(404).send("No such city found !!")
+      return
+    }
+    city.name = cityName
+    city.location = req.body.location
+    city.coordinates = req.body.coordinates
+    await city.save()
+    res.send({ msg: `${city.name} edited successfully` })
+  } catch (error) {
+    res.status(500).send({ error: error.message })
+  }
+}
+
+module.exports = { add, fetchCity, edit }
