2f32f94d10543659d634483a2ef9b5c5d76c1a57
Nirbhay_Elluminati <nirbhay.elluminati@gmail.com>
2023-10-02 20:28:07 +0530

    Revert "Revert "User backend completed successfully""
    This reverts commit 6cacb7c01c500292e135302f8fc35f9620dae09c.

---
+++ src/models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose")
+const validator = require("validator")
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (!/^[a-zA-Z\s'-]+$/.test(value)) throw new Error("Please enter your name properly")
+      },
+    },
+
+    profile: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) throw new Error("Entered email is invalid")
+      },
+    },
+
+    phoneCode: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+
+    phone: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (!/^[0-9]+$/.test(value) || value.length != 10)
+          throw new Error("Enter a valid phone number")
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model("User", userSchema)
